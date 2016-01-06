97e7c86c8cb7ac3e3e51b3ed679a9117083abc92
Tanner <treische@shealy-solutions.com>
2016-01-06 09:42:54 -0500

    Updates #3, Added columns

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -3,3 +3,17 @@
 ---
 CREATE SCHEMA IF NOT EXISTS opportunity_tracker AUTHORIZATION postgres;
 GRANT ALL ON SCHEMA opportunity_tracker TO postgres;
+
+SET SCHEMA 'opportunity_tracker';
+
+---
+--- Create location table
+---
+CREATE TABLE location (
+  id SERIAL PRIMARY KEY,
+  address1 VARCHAR(80) NOT NULL,
+  address2 VARCHAR(80),
+  city VARCHAR(30) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  company_id INTEGER NOT NULL    -- Foreign key set later
+);
