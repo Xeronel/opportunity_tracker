68b787b7dfbfedb59acea08490de919aac51489e
Tanner <treische@shealy-solutions.com>
2016-03-16 12:02:02 -0400

    Add interface

--- otrack.py
+++ otrack.py
@@ -9,16 +9,57 @@ class MainHandler(tornado.web.RequestHandler):
         return self.get_secure_cookie('user')
 
     def get(self):
-        self.render('dashboard.html', title='test')
+        self.render('dashboard.html')
 
     def post(self):
         self.set_secure_cookie('user', self.get_argument('name'))
         self.redirect('/')
 
 
+class Dashboard(tornado.web.RequestHandler):
+    def get(self):
+        self.render('dashboard.html')
+
+
+class Calendar(tornado.web.RequestHandler):
+    def get(self):
+        self.render('calendar.html')
+
+
+class Company(tornado.web.RequestHandler):
+    def get(self):
+        self.render('company.html')
+
+    def post(self):
+        print(self.get_argument('companyname'))
+        print(self.get_argument('industry'))
+        self.render('company.html')
+
+
+class Contact(tornado.web.RequestHandler):
+    def get(self):
+        self.render('contact.html')
+
+
+class Notification(tornado.web.RequestHandler):
+    def get(self):
+        self.render('notification.html')
+
+
+class Note(tornado.web.RequestHandler):
+    def get(self):
+        self.render('note.html')
+
+
 def make_app():
     return tornado.web.Application(
-        [(r'/', MainHandler)],
+        [(r'/', Dashboard),
+         (r'/dashboard', Dashboard),
+         (r'/calendar', Calendar),
+         (r'/add_company', Company),
+         (r'/add_contact', Contact),
+         (r'/add_notification', Notification),
+         (r'/add_note', Note)],
         debug=True,
         autoreload=True,
         compiled_template_cache=False,
--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -1,3 +1,4 @@
+-- noinspection SqlNoDataSourceInspectionForFile
 ---
 --- Create schema
 ---
