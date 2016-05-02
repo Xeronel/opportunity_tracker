7204e50e8e12dbd82ada2cb022edc32341449d85
Tanner <treische@shealy-solutions.com>
2016-05-02 14:57:42 -0400

    Move nav selector code to otrack.js

--- static/otrack.js
+++ static/otrack.js
@@ -2,12 +2,22 @@
 $(".dropdown-menu li a").click(function(){
     var selText = $(this).text();
     $(this).parents('.dropdown').find('.dropdown-toggle').html(selText + ' <span class="caret"></span>');
+    var inputField = $(this).parents('.dropdown').find('input');
+    if (inputField.length == 1) {
+        inputField[0].value = selText;
+    }
 });
 
 // Date picker
-$(function () {
+$(function() {
     $("#datepicker").datepicker({
         autoclose: true,
         todayHighlight: true
     }).datepicker('update', new Date());
 });
+
+// Nav selector
+function navSelector(id) {
+    $(".nav ul li").removeClass("active");
+    $('#'+id.id).addClass('active');
+}
