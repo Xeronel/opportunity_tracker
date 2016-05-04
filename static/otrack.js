7d95e634b8d4ce1463f273b6e40ae68350cc2fc6
Tanner <treische@shealy-solutions.com>
2016-05-04 13:53:38 -0400

    Moved ui code into otrack.js

--- static/otrack.js
+++ static/otrack.js
@@ -21,3 +21,19 @@ function navSelector(id) {
     $(".nav ul li").removeClass("active");
     $('#'+id).addClass('active');
 }
+
+$(function () {
+    if ($('#company').length > 0) {
+        var $company = $('#company').selectize();
+        var company = $company[0].selectize;
+
+        var cookie = Cookies.get('company');
+        if (cookie) {
+            company.setValue(cookie);
+        }
+    }
+});
+
+$(".form-control#company").change(function () {
+    Cookies.set('company', $(this).find('option:selected').val());
+});
