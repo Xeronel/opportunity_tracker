fca20e67331ca30be29d64331b8fa39faf6d06ae
Tanner <treische@shealy-solutions.com>
2016-05-31 09:54:22 -0400

    Override default jquery validator css

--- static/otrack.js
+++ static/otrack.js
@@ -12,6 +12,25 @@ tinymce.init({
     'alignright,alignjustify,bullist,numlist,outdent,indent'
 });
 
+// Validator
+$.validator.setDefaults({
+    highlight: function (element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
 // Dropdown
 $(".dropdown-menu li a").click(function () {
     var selText = $(this).text();
