9f5c168037837a971cdbe046868679b5f9311743
Tanner <treische@shealy-solutions.com>
2016-06-01 15:22:11 -0400

    Add line break

--- page_handlers/__init__.py
+++ page_handlers/__init__.py
@@ -1,4 +1,5 @@
 from .interfaces import BaseHandler
 from .authentication import Login, Logout
 from .pages import Calendar, Dashboard, Company,\
-    Contact, GetContacts, GetNotes, Industry, Note, MainHandler, Notification
+    Contact, GetContacts, GetNotes, Industry, Note, \
+    MainHandler, Notification
