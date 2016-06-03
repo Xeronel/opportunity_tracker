043910c5e6c1a9952d8c9fc133c52d41b9f2011b
Tanner <treische@shealy-solutions.com>
2016-06-03 11:36:35 -0400

    Remove unused function

--- static/otrack.js
+++ static/otrack.js
@@ -31,16 +31,6 @@ $.validator.setDefaults({
     }
 });
 
-// Dropdown
-$(".dropdown-menu li a").click(function () {
-    var selText = $(this).text();
-    $(this).parents('.dropdown').find('.dropdown-toggle').html(selText + ' <span class="caret"></span>');
-    var inputField = $(this).parents('.dropdown').find('input');
-    if (inputField.length == 1) {
-        inputField[0].value = selText;
-    }
-});
-
 // Date picker
 $(function () {
     $("#datepicker").datepicker({
