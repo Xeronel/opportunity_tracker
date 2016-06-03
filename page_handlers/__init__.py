a4de283d247f9dcaadefd0f9470230257717b6b9
Tanner <treische@shealy-solutions.com>
2016-06-03 12:56:13 -0400

    Move admin handler to admin module

--- page_handlers/__init__.py
+++ page_handlers/__init__.py
@@ -3,3 +3,4 @@ from .authentication import Login, Logout
 from .pages import Calendar, Dashboard, Company,\
     Contact, GetContacts, GetNotes, Industry, Note, \
     MainHandler, Notification
+from .admin import Admin
