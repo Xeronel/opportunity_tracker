b3aa89d4c52f93df48d89e23d6da827b8a63a5f2
Tanner <treische@shealy-solutions.com>
2016-08-03 11:19:51 -0400

    Add api endpoint to get notifications by company, Ref #20

--- page_handlers/api.py
+++ page_handlers/api.py
@@ -6,14 +6,29 @@ from tornado import gen
 from dateutil import parser as dateutil
 
 
-class Company(BaseHandler):
+class ApiBase(BaseHandler):
+    def _start_date(self):
+        start_date = self.get_argument('start_date', False)
+        if start_date:
+            start_date = dateutil.parse(start_date).strftime('%Y-%m-%d')
+        return start_date
+
+    def _end_date(self):
+        end_date = self.get_argument('end_date', False)
+        if end_date:
+            end_date = dateutil.parse(end_date).strftime('%Y-%m-%d')
+        return end_date
+
+
+class Company(ApiBase):
     @gen.coroutine
     @tornado.web.authenticated
     def get(self, arg, proc=''):
         rpc = {'': self.company,
                'location': self.location,
                'notes': self.notes,
-               'employee': self.employee}
+               'employee': self.employee,
+               'notifications': self.notifications}
         if proc in rpc:
             result = yield rpc[proc](arg)
             self.write(json_encode(result))
@@ -26,7 +41,8 @@ class Company(BaseHandler):
         rpc = {'': self.company,
                'location': self.location,
                'notes': self.notes,
-               'employee': self.employee}
+               'employee': self.employee,
+               'notifications': self.notifications}
         if proc in rpc:
             result = yield rpc[proc](arg)
             self.write(json_encode(result))
@@ -109,8 +125,41 @@ class Company(BaseHandler):
         else:
             return {}
 
+    @gen.coroutine
+    @tornado.web.authenticated
+    def notifications(self, company_id):
+        if company_id:
+            start_date = self._start_date()
+            end_date = self._end_date()
+            if start_date and end_date:
+                cursor = yield self.db.execute(
+                    "SELECT company.name, first_name, last_name, note, notify_date, sent "
+                    "FROM notification "
+                    "INNER JOIN company "
+                    "ON company.id = notification.company "
+                    "INNER JOIN employee "
+                    "ON notification.employee = employee.id "
+                    "WHERE notification.company=%s "
+                    "AND notify_date >= %s "
+                    "AND notify_date <= %s;",
+                    [company_id, start_date, end_date]
+                )
+            else:
+                cursor = yield self.db.execute(
+                    "SELECT company.name, first_name, last_name, note, notify_date, sent "
+                    "FROM notification "
+                    "INNER JOIN company "
+                    "ON company.id = notification.company "
+                    "INNER JOIN employee "
+                    "ON notification.employee = employee.id "
+                    "WHERE notification.company=%s;",
+                    [company_id])
+            return self.parse_query(cursor.fetchall(), cursor.description)
+        else:
+            return {}
 
-class Contact(BaseHandler):
+
+class Contact(ApiBase):
     @gen.coroutine
     @tornado.web.authenticated
     def get(self, arg, proc=''):
@@ -133,7 +182,7 @@ class Contact(BaseHandler):
             return {}
 
 
-class Employee(BaseHandler):
+class Employee(ApiBase):
     @gen.coroutine
     @tornado.web.authenticated
     def get(self, arg, proc=''):
@@ -235,15 +284,3 @@ class Employee(BaseHandler):
             return self.parse_query(cursor.fetchall(), cursor.description)
         else:
             return {}
-
-    def _start_date(self):
-        start_date = self.get_argument('start_date', False)
-        if start_date:
-            start_date = dateutil.parse(start_date).strftime('%Y-%m-%d')
-        return start_date
-
-    def _end_date(self):
-        end_date = self.get_argument('end_date', False)
-        if end_date:
-            end_date = dateutil.parse(end_date).strftime('%Y-%m-%d')
-        return end_date
