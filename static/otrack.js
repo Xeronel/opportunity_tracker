a4ff94e74dfcf0f41e921b8088e677db9200e66d
dreische <dreische@borderstates.com>
2016-11-02 13:16:27 -0400

    Add datatables search function

--- static/otrack.js
+++ static/otrack.js
@@ -18,6 +18,39 @@ tinymce.init({
     }
 });
 
+// DataTables search function
+jQuery.fn.dataTableExt.oApi.fnFindCellRowIndexes = function ( oSettings, sSearch, iColumn )
+{
+    var
+        i,iLen, j, jLen, val,
+        aOut = [], aData,
+        columns = oSettings.aoColumns;
+
+    for ( i=0, iLen=oSettings.aoData.length ; i<iLen ; i++ )
+    {
+        aData = oSettings.aoData[i]._aData;
+
+        if ( iColumn === undefined )
+        {
+            for ( j=0, jLen=columns.length ; j<jLen ; j++ )
+            {
+                val = this.fnGetData(i, j);
+
+                if ( val == sSearch )
+                {
+                    aOut.push( i );
+                }
+            }
+        }
+        else if (this.fnGetData(i, iColumn) == sSearch )
+        {
+            aOut.push( i );
+        }
+    }
+
+    return aOut;
+};
+
 // Validator
 $.validator.setDefaults({
     highlight: function (element) {
