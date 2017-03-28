c739340370917150f2a4f5ad96415370e3970ac9
Tanner Reische <dreische@borderstates.com>
2017-03-28 14:40:56 -0400

    Implement add cut button

--- static/js/forms/wire_cutting.js
+++ static/js/forms/wire_cutting.js
@@ -16,8 +16,8 @@ wire_cutting.add_reel = function () {
         var row = this.data();
         // If the part and length is the same, add to the qty
         if (row.hasOwnProperty('part_number') &&
-            row.part_number == part && row.length == len) {
-            new_row.qty +=  parseInt(row.qty);
+            row.part_number === part && row.length === len) {
+            new_row.qty += parseInt(row.qty);
             this.remove();
         }
     });
@@ -31,47 +31,67 @@ wire_cutting.add_reel = function () {
 wire_cutting.add_cut = function () {
     var table = $('#cuts').dataTable();
     var part = $('#cut_partnumber').selectize()[0].selectize.getValue();
-    var qty = $('#cut_qty').val();
-    var len = api.v1.part.components(part, function (data) {
-        var result = 0;
+    var qty = parseInt($('#cut_qty').val());
+    var new_row = {
+        part_number: part,
+        qty: qty,
+        length: 0
+    };
+
+    api.v1.part.components(part, function (data) {
+        // Total the number of kit components to get its length
+        // this should be a single wire unless the user made an error
         $.each(data, function (idx, e) {
-            result += e.qty;
+            new_row.length += parseInt(e.qty);
         });
-        return result;
+
+        table.api().rows().every(function () {
+            var row = this.data();
+            // If the part is the same, add to the qty
+            if (row.hasOwnProperty('part_number') && row.part_number === part) {
+                new_row.qty += parseInt(row.qty);
+                this.remove();
+            }
+        });
+
+        if (new_row.part_number) {
+            table.api().row.add(new_row);
+            table.api().draw();
+        }
     });
 };
 
 $(function () {
     var dataTables = [
         {id: '#reels', title: 'Reels:'},
-        {id: '#cuts', title:'Cuts:'}
+        {id: '#cuts', title: 'Cuts:'}
     ];
 
     // Initialize datatables
-    $(dataTables).each(function(idx, e) {
+    $(dataTables).each(function (idx, e) {
         $(e.id).DataTable({
-        sDom: '<"header">frtip',
-        bLengthChange: false,
-        order: [[1, 'asc']],
-        select: {
-            style: 'os'
-        },
-        columnDefs: [
-            {
-                targets: 0,
-                orderable: false,
-                className: 'select-checkbox',
-                defaultContent: ''
-            }
-        ],
-        columns: [
-            {data: null},
-            {data: 'part_number'},
-            {data: 'qty'},
-            {data: 'length'}
-        ]
-    });
-    $('<label class="pull-left" style="font-weight: 700">' + e.title + '</label>')
-        .prependTo(e.id + "_filter");
+            sDom: '<"header">frtip',
+            bLengthChange: false,
+            order: [[1, 'asc']],
+            select: {
+                style: 'os'
+            },
+            columnDefs: [
+                {
+                    targets: 0,
+                    orderable: false,
+                    className: 'select-checkbox',
+                    defaultContent: ''
+                }
+            ],
+            columns: [
+                {data: null},
+                {data: 'part_number'},
+                {data: 'qty'},
+                {data: 'length'}
+            ]
+        });
+        $('<label class="pull-left" style="font-weight: 700">' + e.title + '</label>')
+            .prependTo(e.id + "_filter");
     });
 });
